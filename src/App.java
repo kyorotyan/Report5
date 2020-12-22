caccc969f23b9e8b3750e8a8104473ae4a7a5d8d
Kouki Yanai <e205702@ie.u-ryukyu.ac.jp>
2020-12-23 01:38:08 +0900

    3rd commit

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生した");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
